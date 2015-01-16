cf50c6cb7db16708c985781bb4e06ea6519eaa13
LoiWu <superloiwu@gmail.com>
2015-01-16 18:40:48 +0800

    Loi_Bow Ties (Starter)
    Starter

---
+++ Loi_Bow Ties/Loi_Bow Ties/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Loi_Bow Ties
+//
+//  Created by loi on 1/16/15.
+//  Copyright (c) 2015 GWrabbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var timesWornLabel: UILabel!
+    @IBOutlet weak var lastWornLabel: UILabel!
+    @IBOutlet weak var favoriteLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func segmentedControl(control: UISegmentedControl) {
+        
+    }
+    
+    @IBAction func wear(sender: AnyObject) {
+    }
+    
+    @IBAction func rate(sender: AnyObject) {
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
