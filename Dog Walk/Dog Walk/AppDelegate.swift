e3014d139ee474649a3bbb5ec58b3fdf5221acd9
LoiWu <superloiwu@gmail.com>
2015-01-25 22:03:16 +0800

    Dog Walk (Starter)

---
+++ Dog Walk/Dog Walk/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Dog Walk
+//
+//  Created by Loi Wu on 1/25/15.
+//  Copyright (c) 2015 GWrabbit. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
