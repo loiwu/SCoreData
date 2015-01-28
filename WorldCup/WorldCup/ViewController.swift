ee21ef95639e15d74a5dd916a39aa56f264bdc74
LoiWu <superloiwu@gmail.com>
2015-01-28 15:44:24 +0800

    World Cup
    NSSortDescriptor

--- WorldCup/WorldCup/ViewController.swift
+++ WorldCup/WorldCup/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,NSFetchedResultsControllerDelegate {
 
     var coreDataStack: CoreDataStack!
     
@@ -23,11 +23,17 @@ class ViewController: UIViewController {
         
         //1
         let fetchRequest = NSFetchRequest(entityName: "Team")
-        let sortDescriptor = NSSortDescriptor(key: "teamName", ascending: true)
-        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        let zoneSort = NSSortDescriptor(key: "qualifyingZone", ascending: true)
+        let scoreSort = NSSortDescriptor(key: "wins", ascending: true)
+        let nameSort = NSSortDescriptor(key: "teamName", ascending: true)
+        
+        fetchRequest.sortDescriptors = [zoneSort, scoreSort, nameSort]
         
         //2
-        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: coreDataStack.context, sectionNameKeyPath: "qualifyingZone", cacheName: nil)
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: coreDataStack.context, sectionNameKeyPath: "qualifyingZone", cacheName: "worldCup")
+        
+        fetchedResultsController.delegate = self
         
         //3
         var error: NSError? = nil
@@ -41,51 +47,55 @@ class ViewController: UIViewController {
         return sectionInfo.name
     }
     
-    func numberOfSectionsInTableView
-        (tableView: UITableView) -> Int {
-            
-            return fetchedResultsController.sections!.count
+    func numberOfSectionsInTableView (tableView: UITableView) -> Int {
+        
+        return fetchedResultsController.sections!.count
     }
     
-    func tableView(tableView: UITableView,
-        numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             
         let sectionInfo = fetchedResultsController.sections![section] as NSFetchedResultsSectionInfo
         return sectionInfo.numberOfObjects
     }
     
-    func tableView(tableView: UITableView,
-        cellForRowAtIndexPath indexPath: NSIndexPath)
-        -> UITableViewCell {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let resuseIdentifier = "teamCellReuseIdentifier"
             
-            let resuseIdentifier = "teamCellReuseIdentifier"
-            
-            var cell =
-            tableView.dequeueReusableCellWithIdentifier(
-                resuseIdentifier, forIndexPath: indexPath)
-                as TeamCell
-            
-            configureCell(cell, indexPath: indexPath)
-            
-            return cell
+        var cell = tableView.dequeueReusableCellWithIdentifier(resuseIdentifier, forIndexPath: indexPath) as TeamCell
+        configureCell(cell, indexPath: indexPath)
+        
+        return cell
     }
     
     func configureCell(cell: TeamCell, indexPath: NSIndexPath) {
         let team = fetchedResultsController.objectAtIndexPath(indexPath) as Team
+        
         cell.flagImageView.image = UIImage(named: team.imageName)
         cell.teamLabel.text = team.teamName
         cell.scoreLabel.text = "Wins: \(team.wins)"
     }
     
+    
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-            tableView.deselectRowAtIndexPath(indexPath, animated: true)
-            let team = fetchedResultsController.objectAtIndexPath(indexPath) as Team
-            
-            let wins = team.wins.integerValue
-            team.wins = NSNumber(integer: wins+1)
-            coreDataStack.saveContext()
-            
-            tableView.reloadData()
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let team = fetchedResultsController.objectAtIndexPath(indexPath) as Team
+        
+        let wins = team.wins.integerValue
+        
+        team.wins = NSNumber(integer: wins+1)
+        
+        coreDataStack.saveContext()
+        
+        tableView.reloadData()
+    }
+    
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        
+        tableView.reloadData()
+        
     }
             
 }
